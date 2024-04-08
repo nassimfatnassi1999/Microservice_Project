ee333317777d04debf2dd714f135b65c2cf1eb9c
nassimfatnassi1999 <fatnassihnifi.nassim@esprit.tn>
2024-04-08 01:56:48 +0100

    CRUD works successfully

--- MicroserviceProject/news-service/src/main/java/com/saccess/newsservice/controller/NewsController.java
+++ MicroserviceProject/news-service/src/main/java/com/saccess/newsservice/controller/NewsController.java
@@ -3,9 +3,7 @@ package com.saccess.newsservice.controller;
 import com.saccess.newsservice.entities.News;
 import com.saccess.newsservice.services.IGestionNews;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +16,22 @@ public class NewsController {
     @GetMapping("/getAll")
     public List<News> getAllNews(){
         return news_service.getAllNews();
+    }
+
+    @GetMapping("/get/{id}")
+    public News getNewsById(@PathVariable("id") Long id){
+        return news_service.getNews(id);
+    }
+    @PostMapping("/add")
+    public News addNews(@RequestBody News n){
+       return news_service.addNews(n);
+    }
+    @DeleteMapping("/delete/{id}")
+    public void deleteNews(@PathVariable("id") Long id){
+        news_service.deleteNews(id);
+    }
+    @PutMapping("/edit/{id}")
+    public News updateNews(@RequestBody News n,@PathVariable("id") Long id){
+        return news_service.updateNews(n,id);
     }
 }

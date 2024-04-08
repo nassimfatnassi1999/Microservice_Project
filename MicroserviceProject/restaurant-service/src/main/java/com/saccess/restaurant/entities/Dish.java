1e5f5733e5cb3ae091f05e1a3c6dcf4e8ba47fb4
Mohamed Ali Naguez <mohamedali.naguez@esprit.tn>
2024-04-08 12:12:14 +0100

    Restaurant Service Entities, services , repositories , controllers created

---
+++ MicroserviceProject/restaurant-service/src/main/java/com/saccess/restaurant/entities/Dish.java
@@ -0,0 +1,30 @@
+package com.saccess.restaurant.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Dish {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_dish;
+    private String name;
+    private String description;
+    private float price;
+    private String photo;
+    private int id_menu;
+    private String category;
+    // Add ingredients, preparation time, availability, dietary information, calories/nutritional information if necessary
+
+    @ManyToOne
+    @JoinColumn(name = "id_restaurant")
+    private Restaurant restaurant;
+}

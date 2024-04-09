c2c8114e5b765fa64e3fbe99a0cd0fcda2aea1d0
YoussefAyed <ayedy40@gmail.com>
2024-04-09 16:30:12 +0100

    Application fix

--- MicroserviceProject/user-service/src/main/java/com/saccess/user/UserServiceApplication.java
+++ MicroserviceProject/user-service/src/main/java/com/saccess/user/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.saccess.user.service;
+package com.saccess.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

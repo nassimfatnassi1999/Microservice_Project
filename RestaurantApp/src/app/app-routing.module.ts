57d6778be11c1487c3dc91f5f82d5fe228836182
nassimfatnassi1999 <fatnassihnifi.nassim@esprit.tn>
2024-04-17 20:50:26 +0100

    template navbar+home integrer

--- RestaurantApp/src/app/app-routing.module.ts
+++ RestaurantApp/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NewsCompoenentComponent } from './news-compoenent/news-compoenent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'news', component: NewsCompoenentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

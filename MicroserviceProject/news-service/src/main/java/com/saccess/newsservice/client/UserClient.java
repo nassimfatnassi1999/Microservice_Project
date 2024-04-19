420cce997df87657fc5800a4daa69b81f575ac9f
nassimfatnassi1999 <fatnassihnifi.nassim@esprit.tn>
2024-04-19 22:31:37 +0100

    before merge with ala :feignClient

--- MicroserviceProject/news-service/src/main/java/com/saccess/newsservice/client/UserClient.java
+++ MicroserviceProject/news-service/src/main/java/com/saccess/newsservice/client/UserClient.java
@@ -1,7 +1,14 @@
 package com.saccess.newsservice.client;
 
+import com.saccess.newsservice.dto.UserDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @FeignClient(value = "USER-SERVICE")
 public interface UserClient {
+
+    @GetMapping("/user/getAllUsers")
+    List<UserDto> getAllUsers();
 }

dc825450d79fc93cceed5378a88aa55e8d84ea7b
HamdiAlaEddin <alaaeddin.hamdi@esprit.tn>
2024-04-19 22:17:25 +0100

    feignclint success with user

--- MicroserviceProject/feedBack-service/src/main/java/com/saccess/feedBack/repositories/IFeedBackRepository.java
+++ MicroserviceProject/feedBack-service/src/main/java/com/saccess/feedBack/repositories/IFeedBackRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.Scanner;
 import org.springframework.data.jpa.repository.Query;
 import java.time.LocalDate;
@@ -12,8 +13,8 @@ import java.util.List;
 import org.springframework.data.repository.query.Param;
 @Repository
 public interface IFeedBackRepository extends JpaRepository<Feedback,Long> {
-    @Query("SELECT f FROM Feedback f WHERE f.CreatedAt = :CreateDate")
-    public List<Feedback> findDateCreation(@Param("CreatedAt") LocalDate CreatDate);
+
+
 
 
 }

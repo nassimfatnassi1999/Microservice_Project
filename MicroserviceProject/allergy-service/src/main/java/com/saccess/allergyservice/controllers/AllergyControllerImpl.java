6cf69b05bdc2835585f11edf842c3f1255fcef5b
mohamed achref <mohmedachref.samaali@esprit.tn>
2024-04-20 01:30:48 +0200

    after communication micro-service

--- MicroserviceProject/allergy-service/src/main/java/com/saccess/allergyservice/controllers/AllergyControllerImpl.java
+++ MicroserviceProject/allergy-service/src/main/java/com/saccess/allergyservice/controllers/AllergyControllerImpl.java
@@ -1,11 +1,13 @@
 package com.saccess.allergyservice.controllers;
 
+import com.saccess.allergyservice.dto.Userdto;
 import com.saccess.allergyservice.entities.Allergy;
 import com.saccess.allergyservice.entities.Level;
 import com.saccess.allergyservice.services.IGestionAllergy;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -25,6 +27,10 @@ IGestionAllergy gestionAllergy;
     public Allergy updateAllergy(@RequestBody Allergy allergy){
         return gestionAllergy.updateAllergy(allergy);
     }
+    @PostMapping("/addAllergy")
+    public Allergy AddAllergy(@RequestBody Allergy allergy){
+        return gestionAllergy.addAllergy(allergy);
+    }
     @DeleteMapping("/deleteallergy/{id}")
     public void deleteallergy(@PathVariable("id") Long id_allergy){
         gestionAllergy.removeAllergy(id_allergy);
@@ -37,5 +43,13 @@ IGestionAllergy gestionAllergy;
     @GetMapping("/getAllergybyLevel/{level}")
     public List<Allergy> getAllergyBylevel(@PathVariable("level")Level level){
         return  gestionAllergy.getAllergyLevel(level);
+    }
+    @GetMapping("/getUserById/{id}")
+    public Userdto getUserById(@PathVariable("id")Long id){
+        return gestionAllergy.findUserById(id) ;
+    }
+    @GetMapping("/getTotalAll/{datede}/{datefin}")
+    public int  getTotalAllergiesByDateRange(@PathVariable("datede") LocalDate datedebut,@PathVariable("datefin") LocalDate dateFin){
+        return gestionAllergy.getTotalAllergiesByDateRange(datedebut,dateFin) ;
     }
 }

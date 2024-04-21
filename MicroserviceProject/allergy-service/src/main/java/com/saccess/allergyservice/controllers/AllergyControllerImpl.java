3ae01783047a3780cf0a0ca822f82147d111f4fa
mohamed achref <mohmedachref.samaali@esprit.tn>
2024-04-21 18:37:33 +0200

    add GetewayProperties

--- MicroserviceProject/allergy-service/src/main/java/com/saccess/allergyservice/controllers/AllergyControllerImpl.java
+++ MicroserviceProject/allergy-service/src/main/java/com/saccess/allergyservice/controllers/AllergyControllerImpl.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/Allergy")
+@RequestMapping("/apiachref/Allergy")
 @CrossOrigin("*")
 @AllArgsConstructor
 public class AllergyControllerImpl {

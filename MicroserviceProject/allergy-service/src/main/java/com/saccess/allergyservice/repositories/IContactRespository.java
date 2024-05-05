610b4e88f6253352a9117de8beb2e4e62e6834e9
mohamed achref <mohmedachref.samaali@esprit.tn>
2024-05-05 15:22:32 +0200

    send mail contact

--- MicroserviceProject/allergy-service/src/main/java/com/saccess/allergyservice/repositories/IContactRespository.java
+++ MicroserviceProject/allergy-service/src/main/java/com/saccess/allergyservice/repositories/IContactRespository.java
@@ -4,6 +4,9 @@ import com.saccess.allergyservice.entities.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IContactRespository extends JpaRepository<Contact,Long> {
+    List<Contact> findAllByOrderByDateDesc();
 }
